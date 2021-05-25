023e9629f02f5db8824891b8485cbd166ea9d8d7
torcy-it <83276582+torcy-it@users.noreply.github.com>
2021-05-25 14:24:07 +0200

    Update interfaccia_1_3.c

--- Interfaccia grafica/interfaccia_1_3.c
+++ Interfaccia grafica/interfaccia_1_3.c
@@ -1,14 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
-#include "hlista.h"
+#include <time.h>
 
-void interfaccia_merce ( );
+#define FIELD_SIZE 25
+
+#include "hlista.h"
+#include "hour_function.h"
+
+void interfaccia_merce ( lista );
 char * merce_scelte ( int  );
+
+void interfaccia_login ( );
+char * login_scelte ( int  );
+
+void login ( lista *  , lista * );
+lista * signin ( lista *  );
 
 int main ( )
 {
-    interfaccia_merce ( );
+    interfaccia_login ( );
     
     return 0;
 }
@@ -24,7 +35,7 @@ char * merce_scelte ( int scelta )
 
 }
 
-void interfaccia_merce ( )
+void interfaccia_merce ( lista utente )
 {   
     int ch = 0;
     int scelta = 0;
@@ -50,6 +61,8 @@ void interfaccia_merce ( )
         print_lista_merce ( lista_spesa );
 
         printf("\n\t\tNULL\n\n");
+
+        printf("\t\tBENVENUTO %s %s \n\n",utente.info_user.name_user, utente.info_user.surname_user);
 
 
         if( ch == 75 ) scelta = scelta - 1;
@@ -148,6 +161,7 @@ void interfaccia_merce ( )
 
     }while ( !exit );  
 
+    aggiorna_merce ( lista_merce );
 
 }
 
@@ -155,3 +169,93 @@ void interfaccia_merce ( )
 // perchè non sono riuscito ad emularla
 // per cambiare la printf vai su clista.c e cambi la funzione 
 //print_merce che stampa la lista fino a quando non e' null
+char *login_scelte ( int scelta )
+{
+    if ( scelta == 0) return "log";
+    else if ( scelta == 1 ) return "sign";
+    else return "exit";
+
+}
+
+
+void interfaccia_login ( )
+{
+    int ch = 0;
+    int scelta = 0;
+    bool exit = false;
+    lista utente;
+
+
+    lista * lista_user = preleva_dati_da_file ( fopen ("utenti.txt","r"), false);
+
+    do
+    {
+
+        system("cls");
+
+        printf("\n\n\n");
+        if( ch == 75 ) scelta = scelta - 1;
+
+        if( ch == 77 ) scelta = scelta + 1;
+
+        if ( scelta < 0 ) scelta = 2;
+
+        if ( scelta > 2 ) scelta = 0;
+
+
+        for ( int i = 0; i < 3; i++ )
+        {
+            if ( i == scelta ) printf ("\t [ %s ] ", login_scelte( i ) );
+            
+            else printf("\t%s ", login_scelte( i ));
+        }
+        printf("\n\n\n\n\t");
+        
+        ch = getch( );
+
+
+        if( ( ch == '\r') )
+        {
+            if( scelta == 0 ) 
+            {
+                
+                system("cls");
+
+                login ( lista_user, &utente );
+
+                
+                if( utente.info_user.carico_veivolo >= 0 )
+                {
+                    interfaccia_merce ( utente );
+                }
+                else
+                {
+                    printf("\n\n\t\tLOGIN FAILED\n\n\n");
+                    system("pause");
+                }
+
+            }
+            
+            if( scelta == 1 )
+            {
+                system("cls");
+                lista_user = signin ( lista_user );
+
+            }
+
+            if( scelta == 2 )
+            {
+                exit = true;
+            }
+            system ("cls"); 
+
+
+        }
+
+
+    }while ( !exit );  
+
+    aggiorna_user_su_file ( lista_user, fopen("utenti.txt","w"));
+    aggiorna_file_merce ( );
+
+}
