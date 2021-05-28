3773ebd7bf1556ca7923f3a323b0606db08713e9
torcy-it <83276582+torcy-it@users.noreply.github.com>
2021-05-28 17:56:07 +0200

    Create print_graph.c

---
+++ Interfaccia grafica/print_graph.c
@@ -0,0 +1,36 @@
+void stampa_grafo()
+{
+    printf("\n\n");
+    printf("                    ___                   ___\n");
+    printf("                   /\xB0\xB1\xB0\\       100       /\xB0\xB1\xB0\\\n");
+    printf("                  |\xB0\xB1%c\xB1\xB0|---------------|\xB0\xB1%c\xB1\xB0|\n",'A','B');
+    printf("                   \\\xB0\xB1\xB0/                 \\\xB0\xB1\xB0/\n");
+    printf("                   /    \\               /    \\ \n");
+    printf("                  /      \\             /      \\ \n");
+    printf("                 /        \\           /        \\ \n");
+    printf("                /       50 \\         / 100      \\ \n");
+    printf("               /            \\       /            \\ \n");
+    printf("           40 /              \\     /              \\ 40\n");
+    printf("             /                \\   /                \\ \n");
+    printf("            /                 /\xB0\xB1\xB0\\                 \\ \n");
+    printf("           /                 |\xB0\xB1%c\xB1\xB0|                 \\ \n",'C');
+    printf("          /                   \\\xB0\xB1\xB0/                   \\ \n");
+    printf("         /         100        / | \\         60         \\ \n");
+    printf("      /\xB0\xB1\xB0\\------------------'  |  '------------------/\xB0\xB1\xB0\\      \n");
+    printf("     |\xB0\xB1%c\xB1\xB0|                  30|                    |\xB0\xB1%c\xB1\xB0|\n",'D','E');
+    printf("      \\\xB0\xB1\xB0/-----------------.   |   .-----------------\\\xB0\xB1\xB0/\n");
+    printf("         \\         100       \\  |  /       100         / \n");
+    printf("          \\                   /\xB0\xB1\xB0\\                   / \n");
+    printf("           \\                 |\xB0\xB1%c\xB1\xB0|                 / \n",'F');
+    printf("            \\                 \\\xB0\xB1\xB0/                 / \n");
+    printf("             \\                /   \\                /\n");
+    printf("           60 \\              /     \\              / 100\n");
+    printf("               \\            /       \\            /\n");
+    printf("                \\       40 /         \\ 90       /\n");
+    printf("                 \\        /           \\        /\n");
+    printf("                  \\      /             \\      /\n");
+    printf("                   \\    /               \\    /\n");
+    printf("                   /\xB0\xB1\xB0\\                 /\xB0\xB1\xB0\\      \n");
+    printf("                  |\xB0\xB1%c\xB1\xB0|---------------|\xB0\xB1%c\xB1\xB0|\n",'G','H');
+    printf("                   \\\xB0\xB1\xB0/       100       \\\xB0\xB1\xB0/\n");
+}
